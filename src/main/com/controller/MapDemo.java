b10ed28ba7bc3c4c076f7ea8711ce1c0654e0f79
韩星 <467527309@qq.com>
2018-07-24 13:39:12 +0800

    对map有了更加深刻的理解

--- src/main/com/controller/MapDemo.java
+++ src/main/com/controller/MapDemo.java
@@ -1,4 +1,4 @@
-package controller;
+package com.controller;
 
 
 import java.util.HashMap;
@@ -9,7 +9,7 @@ import java.util.Set;
 public class MapDemo {
 
     public static void main(String[] args) {
-        Map<String, Integer> map = new HashMap<String, Integer>();
+        Map <String, Integer> map = new HashMap <String, Integer>();
         // 添加值
         map.put("a", 1);
         map.put("b", 1);
@@ -39,28 +39,56 @@ public class MapDemo {
 //            System.out.println(i + "..." + value);
 //        }
 
+
         /**
          * 1。调用map中的entrySet将集合中的集合映射关系，存储到Set集合
          * 2。迭代set集合
          * 3。获取集合元素是映射关系
          * 4。通过映射对象方法getkey,getValue获取健值对
          */
+//
+//        Set<Map.Entry<String, Integer>> entryset = map.entrySet();
+//        Iterator<Map.Entry<String, Integer>> it = entryset.iterator();
+//        while (it.hasNext()) {
+//            Map.Entry<String, Integer> entry = it.next();
+//            String key = entry.getKey();
+//            Integer value = entry.getValue();
+//            System.out.println(key + "..." + value);
+//        }
+//
+//        for(Map.Entry<String, Integer> entry:map.entrySet()){
+//            String key = entry.getKey();
+//            Integer value = entry.getValue();
+//            System.out.println(key + "..." + value);
+//        }
 
-        Set<Map.Entry<String, Integer>> entryset = map.entrySet();
-        Iterator<Map.Entry<String, Integer>> it = entryset.iterator();
-        while (it.hasNext()) {
-            Map.Entry<String, Integer> entry = it.next();
-            String key = entry.getKey();
-            Integer value = entry.getValue();
-            System.out.println(key + "..." + value);
+
+        /**
+         * map 四种遍历方法
+         * 增强for循环遍历 keySet()   entrySet()
+         * 迭代器遍历  使用keySet()遍历  使用entrySet()遍历
+         */
+
+        for (String key : map.keySet()) {
+            System.out.println(key + ":" + map.get(key));  // 31 ms
         }
 
-        for(Map.Entry<String, Integer> entry:map.entrySet()){
-            String key = entry.getKey();
-            Integer value = entry.getValue();
-            System.out.println(key + "..." + value);
+        for (Map.Entry <String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());  // 20 ms
         }
 
+        Iterator <String> iterator = map.keySet().iterator();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            Integer value = map.get(key);
+            System.out.println(key + ":" + value);   //17 ms
+        }
+
+        Iterator <Map.Entry <String, Integer>> iterator1 = map.entrySet().iterator();
+        while (iterator1.hasNext()) {
+            Map.Entry <String, Integer> entry = iterator1.next();
+            System.out.println(entry.getKey() + ":" + entry.getValue());  //10ms
+        }
 
 
     }
